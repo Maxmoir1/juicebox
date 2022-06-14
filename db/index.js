f9647d65214d96f47004e8fba02d98fbe872e902
MaxMoir <kooksaga@gmail.com>
2022-06-13 20:59:28 -0700

    day 3 finished

--- db/index.js
+++ db/index.js
@@ -51,6 +51,46 @@ async function updateUser(id, fields = {}) {
         throw error;
     }
 }
+async function getPostById(postId) {
+    try {
+        const { rows: [post] } = await client.query(`
+      SELECT *
+      FROM posts
+      WHERE id=$1;
+    `, [postId]);
+
+        // THIS IS NEW
+        if (!post) {
+            throw {
+                name: "PostNotFoundError",
+                message: "Could not find a post with that postId"
+            };
+        }
+        // NEWNESS ENDS HERE
+
+        const { rows: tags } = await client.query(`
+      SELECT tags.*
+      FROM tags
+      JOIN post_tags ON tags.id=post_tags."tagId"
+      WHERE post_tags."postId"=$1;
+    `, [postId])
+
+        const { rows: [author] } = await client.query(`
+      SELECT id, username, name, location
+      FROM users
+      WHERE id=$1;
+    `, [post.authorId])
+
+        post.tags = tags;
+        post.author = author;
+
+        delete post.authorId;
+
+        return post;
+    } catch (error) {
+        throw error;
+    }
+}
 
 async function getAllUsers() {
     try {
@@ -101,6 +141,9 @@ async function createPost({
       VALUES($1, $2, $3)
       RETURNING *;
     `, [authorId, title, content]);
+
+        const tagList = await createTags(tags);
+
 
         return post;
     } catch (error) {
--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 const apiRouter = require('./api');
+
 server.use('/api', apiRouter);
+server.get('/add/:first/to/:second', (req, res, next) => {
+    res.send(`<h1>${req.params.first} + ${req.params.second} = ${Number(req.params.first) + Number(req.params.second)
+        }</h1>`);
+});
+server.get('/background/:color', (req, res, next) => {
+    res.send(`
+    <body style="background: ${req.params.color};">
+      <h1>Hello World</h1>
+    </body>
+  `);
+});
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
