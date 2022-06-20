e5d8f1cfa3374e6dad22e4c2f3806e95fb7194e2
MaxMoir <kooksaga@gmail.com>
2022-06-20 12:35:34 -0700

    heroku push

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const { client } = require('./db');
 client.connect();
-const PORT = 3000;
+const { PORT = 3000 } = process.env
 const express = require('express');
 const server = express();
 const apiRouter = require('./api');
 
 server.use('/api', apiRouter);
 server.get('/add/:first/to/:second', (req, res, next) => {
-    res.send(`<h1>${req.params.first} + ${req.params.second} = ${Number(req.params.first) + Number(req.params.second)
-        }</h1>`);
+  res.send(`<h1>${req.params.first} + ${req.params.second} = ${Number(req.params.first) + Number(req.params.second)
+    }</h1>`);
 });
 server.get('/background/:color', (req, res, next) => {
-    res.send(`
+  res.send(`
     <body style="background: ${req.params.color};">
       <h1>Hello World</h1>
     </body>
@@ -25,13 +25,13 @@ server.use(morgan('dev'));
 server.use(express.json())
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
 
-    next();
+  next();
 });
 
 server.listen(PORT, () => {
-    console.log('The server is up on port', PORT)
+  console.log('The server is up on port', PORT)
 });
